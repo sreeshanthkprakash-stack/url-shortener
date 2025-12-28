a4bcc66c27ebfad1d53fe41b3d09cdb6692a5674
sreeshanthkprakash <sreeshanthkprakash@gmail.com>
2025-12-28 20:42:56 +0530

    update

--- controllers/urlcontrollers.js
+++ controllers/urlcontrollers.js
@@ -1,3 +1,38 @@
+const Url = require("../models/url");
+const shortid = require("shortid");
+const QRCode = require("qrcode");
+
+exports.createShortUrl = async (req, res) => {
+  try {
+    const { originalUrl } = req.body;
+
+    if (!originalUrl || !originalUrl.startsWith("http")) {
+      return res.status(400).json({ error: "Invalid URL" });
+    }
+
+    const shortCode = shortid.generate();
+    const shortUrl = `${req.protocol}://${req.get("host")}/${shortCode}`;
+    const qrImage = await QRCode.toDataURL(shortUrl);
+
+    const newUrl = new Url({ originalUrl, shortCode, qrCode: qrImage, clicks: 0 });
+    await newUrl.save();
+
+    res.json({ shortUrl, qrCode: qrImage });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Server error" });
+  }
+};
+
+exports.getRecentUrls = async (req, res) => {
+  try {
+    const urls = await Url.find().sort({ _id: -1 }).limit(5);
+    res.json(urls);
+  } catch (err) {
+    res.status(500).json({ error: "Server error" });
+  }
+};
+
 exports.redirectUrl = async (req, res) => {
   try {
     const code = req.params.code;
--- routes/urlroutes.js
+++ routes/urlroutes.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const router = express.Router();
+
 const { createShortUrl, redirectUrl, getRecentUrls } = require("../controllers/urlcontrollers");
 
 router.post("/shorten", createShortUrl);
 router.get("/recent", getRecentUrls);
-
-// 🔥 THIS MUST BE LAST
 router.get("/:code", redirectUrl);
 
 module.exports = router;
