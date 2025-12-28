fafbababf2411471dcf0b8fbef003b4dcb0baab0
sreeshanthkprakash <sreeshanthkprakash@gmail.com>
2025-12-28 20:24:11 +0530

    update

--- routes/urlroutes.js
+++ routes/urlroutes.js
@@ -1,13 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { createShortUrl, redirectUrl } = require("../controllers/urlcontrollers");
-const { getRecentUrls } = require("../controllers/urlcontrollers");
+const { createShortUrl, redirectUrl, getRecentUrls } = require("../controllers/urlcontrollers");
 
 router.post("/shorten", createShortUrl);
-router.get("/:code", redirectUrl);
 router.get("/recent", getRecentUrls);
 
+// 🔥 THIS MUST BE LAST
+router.get("/:code", redirectUrl);
 
 module.exports = router;
-
-
